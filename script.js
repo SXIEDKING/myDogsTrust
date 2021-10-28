d5659765daec7035dcc219085e963af8f3c70e42
SaiedIslam <saiedislam61@gmail.com>
2021-10-28 01:26:16 +0100

    added

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 let dogs=[]
 
 
-dogs.push({name:"Fluffy",breed:"Poodle",picture:"",age:12})
-dogs.push({name:"Otto",breed:"Chihuahua",picture:"",age:14})
-dogs.push({name:"Padre",breed:"Greyhound",picture:"",age:19})
-dogs.push({name:"Louwee",breed:"American Pitbull Terrier",pictures:"",age:22})
-dogs.push({name:"Snoopy",breed:"Husky",pictures:"",age:20})
-dogs.push({name:"Kaiser",breed:"German Sheperd",pictures:"",age:20})
+dogs.push({name:"Fluffy",breed:"Poodle",picture:"",age:7})
+dogs.push({name:"Otto",breed:"Chihuahua",picture:"",age:8})
+dogs.push({name:"Padre",breed:"Greyhound",picture:"",age:9})
+dogs.push({name:"Louwee",breed:"American Pitbull Terrier",pictures:"",age:9})
+dogs.push({name:"Snoopy",breed:"Husky",pictures:"",age:9})
+dogs.push({name:"Kaiser",breed:"German Sheperd",pictures:"",age:9})
 
 //6 dogs - on 300*400 pixel cards (with nice padding and margins, and a nice background colour)
 // An image for each dog
@@ -20,22 +20,31 @@ for (let i=0; i<dogs.length; i++){
 
     let card = document.createElement("div")
     document.body.appendChild(card)
+
     let dogName = document.createElement("h1")
     card.appendChild(dogName)
-    document.body.style.backgroundColor = "yellow" //Changes background colour
-    card.style.backgroundColor = "blue" //Changes colour of the card
+
+    document.body.style.backgroundColor = "white" //Changes background colour
+    card.style.backgroundColor = "yellow" //Changes colour of the card
+
     let dogBreed = document.createElement("h2")
     card.appendChild(dogBreed)
+
     let dogPicture = document.createElement("img")
     card.appendChild(dogPicture)
     dogPicture.src = dogs[i].picture
+
     let dogAge = document.createElement("p")
     card.appendChild(dogAge)
-    card.style.padding = 300 + "px"
-    card.style.margin = 400 + "px"
+    card.style.padding = 5 + "px"
+    card.style.margin = 5 + "px"
+    
     dogName.innerHTML = dogs[i].name
+
     dogBreed.innerHTML = dogs[i].breed
+
     dogPicture.innerHTML = dogs[i].picture
+
     dogAge.innerHTML = dogs[i].age
 }
 
